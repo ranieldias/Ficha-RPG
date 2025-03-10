a7902ed0c775bfc5964d6103f41965f578416cea
Raniel Dias <oranieldias@gmail.com>
2025-03-10 02:57:19 -0300

    Pay atention 2

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ function updateStatus(sheetId) {
 
 function sendUpdateRequest(cell, value) { 
     // Retriving selected sheet
-    const savedValue = localStorage.getItem('selectedDropdownValue');
+    const savedValue = "1h1YwJkq2dw9CO_rEPFVJMULT-cVCNHO8nnWt5leJ4VE";
     document.getElementById('retrieve').textContent = savedValue;
     
     const url = `https://script.google.com/macros/s/AKfycbx727Wws4Axs7qRAiJ9wHV8GmgiMo8SV_qhqjRvvsJUxtcpWEnjR7EHE3e5TB-oxtQLiA/exec?spreadsheetId=${savedValue}&cell=${cell}&value=${value}`;
